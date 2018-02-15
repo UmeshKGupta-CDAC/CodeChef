d994ffeac9fcb47c0ebc9f37997012b258071037
umeshkumar1991 <umesh.kumar@tothenew.com>
2018-02-16 03:34:49 +0530

    Trailing Zeros in factorial solution done.

--- src/com/ukworld/codechef/easy/TrailingZerosInFactorial.java
+++ src/com/ukworld/codechef/easy/TrailingZerosInFactorial.java
@@ -1,5 +1,9 @@
 package com.ukworld.codechef.easy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Problem Name and Code: Factorial(FCTRL)
  * problem link: https://www.codechef.com/problems/FCTRL
@@ -7,4 +11,19 @@ package com.ukworld.codechef.easy;
  */
 public class TrailingZerosInFactorial {
 
+  public static void main(String[] args) throws IOException {
+    final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in),
+        32768);
+    int testCases = Integer.parseInt(bufferedReader.readLine());
+    long n;
+    while (testCases-- > 0) {
+      n = Long.parseLong(bufferedReader.readLine());
+      int noOfTrailingZero = 0;
+      while (n > 0) {
+        n = n / 5;
+        noOfTrailingZero += n;
+      }
+      System.out.println(noOfTrailingZero);
+    }
+  }
 }
